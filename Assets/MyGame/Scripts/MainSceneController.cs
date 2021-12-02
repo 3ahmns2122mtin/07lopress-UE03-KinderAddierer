8f881b2fc2210b221186f11e83653e18769ce5c8
lopress <90834282+lopress@users.noreply.github.com>
2021-12-02 10:32:22 +0100

    Stand in der Klasse / 02.12.2021

---
+++ Assets/MyGame/Scripts/MainSceneController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class MainSceneController : MonoBehaviour
+{
+    [SerializeField] private InputField inputFieldTermA, inputFieldTermB;
+    [SerializeField] private Text txtResult;
+    public bool testing;
+
+    public int CheckAddition(int summandA, int summandB)
+    {
+        int result = 0;
+        testing = false;
+
+        result = summandA + summandB;
+
+        return result;
+    }
+
+    void Start()
+    {
+        int val1 = CheckAddition(1,2);
+        int val2 = CheckAddition(10,20);
+        int val3 = CheckAddition(100,200);
+    }
+
+    public void GetValues()
+    {
+        string termA, termB;
+        termA = inputFieldTermA.text;
+        termB = inputFieldTermB.text;
+
+        Debug.Log("Methode GetValues wird ausgeführt: " + CheckAddition(int.Parse(termA), int.Parse(termB)));
+    }
+
+    void Update()
+    {
+        if (testing)
+        {
+            GetValues();
+        }
+    }
+}
